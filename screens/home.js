2b24dcf27afa05950bd78fda57d7b22b0b467485
Ryan Paige <oshmagoshzz@gmail.com>
2021-01-12 18:58:22 -0500

    api usage to retrieve scores and comments added

--- screens/home.js
+++ screens/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
@@ -6,9 +6,13 @@ import { Card, ListItem, Icon } from 'react-native-elements';
 import ScoreCard from '../components/scoreCard';
 import Button from '../components/buttons';
 
+//consistent screen dimensions across multiple devices
 const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
+
+// imported nodejs nba api from https://github.com/bttmly/nba
 const NBA = require("nba");
 
+//Initial object to use before the nba api's async is fulfilled
 const initialState = [
   {
     "ast": 0,
@@ -21,20 +25,28 @@ const initialState = [
   },
 ];
 
+// Promise from nba api to retrieve nested objects/arrays and get specific stats needed
+const todaysGames = NBA.stats.scoreboard({gameDate: "01/12/2021"}).then(res => console.log(res.gameHeader));
 
-const Home = () => (
-  <>
-      <Card containerStyle={styles.titleContainer}> 
-        <Card.Title>Today's Scores</Card.Title>
-        <Card.Divider style={styles.divider} />
-          <Text style={{alignSelf:'center', marginBottom: 10}}>
-            Scores From :Input Date
-          </Text>
-      </Card>
 
-      <ScoreCard item={initialState}/>
-  </>      
-  );
+const Home = () => {
+  // useState hook for future changing of default initialState to nba api's data
+  const [state, setstate] = useState(initialState);
+
+  return(
+      <>
+        <Card containerStyle={styles.titleContainer}> 
+          <Card.Title>Today's Scores</Card.Title>
+          <Card.Divider style={styles.divider} />
+            <Text style={{alignSelf:'center', marginBottom: 10}}>
+              Scores From :Input Date
+            </Text>
+        </Card>
+        {/* scorecard list component showcasing Today's scores*/}
+        <ScoreCard item={state}/>
+    </>   
+  )   
+};
 
 export default Home;
 
