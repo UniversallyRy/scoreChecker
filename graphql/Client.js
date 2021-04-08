717b7b404e5980dace0f3bb7d9f0a2b5d15d3716
Ryan Paige <oshmagoshzz@gmail.com>
2021-04-08 15:22:41 -0400

    hiddenkeyy

--- graphql/Client.js
+++ graphql/Client.js
@@ -1,11 +1,12 @@
 import { ApolloClient, createHttpLink } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { RestLink } from "apollo-link-rest";
+import apiKey from "../.expo/apiKey";
 
 const restLink = new RestLink({
   uri: "free-nba.p.rapidapi.com",
   headers: {
-    Authorization: "45f39269e3msh09e384327c9ebf3p1eccfbjsna51b9a9e6a21",
+    Authorization: apiKey,
   },
 });
 
