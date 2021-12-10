41fb7fd87df5d8386f24777f2a089aeac21894fb
Ryan Paige <oshmagoshzz@gmail.com>
2021-12-10 14:38:10 -0500

    format

--- constants.js
+++ constants.js
@@ -9,14 +9,14 @@ export const DEFAULT_PLAYER_INFO = {
 };
 
 export const colorScheme = {
-  //  Background color Ochre
+  //Background color Ochre
   background: "#273e47",
-  //  Top/Naviagtion/Container color International Orange Golden Gate Bridge
+  //Top/Naviagtion/Container color International Orange Golden Gate Bridge
   foreground: "#C32F27",
-  //  Text color Maximum Yellow Red
+  //Text color Maximum Yellow Red
   text: "#F7B538",
-  //  Button color UP Maroon
+  //Title color UP Maroon
   title: "#780116",
-  // Divider color Flame
+  //Divider color Flame
   divider: "#D8572A",
 };
