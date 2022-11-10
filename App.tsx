bdb811d0ac8e16475dfe1e3fa52965fdc9247b25
Ryan Paige <universallyry@gmail.com>
2022-11-09 21:28:48 -0500

    babel changes

--- App.tsx
+++ App.tsx
@@ -64,7 +64,7 @@ export const App = () => {
           onLayout={onLayoutRootView}
         >
           <StatusBar style="auto" />
-          <BottomTabs />
+          <StorybookUIRoot />
         </Flex>
       </NavigationContainer>
     </NativeBaseProvider>
